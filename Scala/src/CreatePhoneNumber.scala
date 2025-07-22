cb68572007ecf23a92e028313d8e445811d74631
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-07-22 13:08:09 +0200

    Init

---
+++ Scala/src/CreatePhoneNumber.scala
@@ -0,0 +1,15 @@
+object CreatePhoneNumber extends App{
+//  Description:
+//  Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+//  Example
+//
+//  createPhoneNumber(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)) # => returns "(123) 456-7890"
+
+  def createPhoneNumber(numbers: Seq[Int]): String = {
+    val groups = numbers.grouped(3).toList.map(x => x.mkString)
+    s"(${groups(0)}) ${groups(1)}-${groups(2)}${groups(3)}"
+  }
+
+  println(createPhoneNumber(Seq(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)))
+}
+
