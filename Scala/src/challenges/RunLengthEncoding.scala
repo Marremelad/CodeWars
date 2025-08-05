885cbc0df420050bf4f98b7bf36b995cd4fbf855
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-08-05 17:00:21 +0200

    Run length encoding

--- Scala/src/challenges/RunLengthEncoding.scala
+++ Scala/src/challenges/RunLengthEncoding.scala
@@ -3,24 +3,22 @@ package challenges
 import scala.annotation.tailrec
 
 object RunLengthEncoding extends App {
+  val s = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
 
-  def rle(s1: String): String = {
+  def rle(s: String): String = {
     @tailrec
-    def grouper(s2: String, previous: Char, acc: String = "", result: List[String] = Nil): List[String] = {
-      // Reverse the list of groups, after prepending, to get the correct order.
-      if (s2.length <= 0) (acc :: result).reverse
+    def rec(remaining: String, acc: List[String]): List[String] = {
+      if (remaining.isEmpty) acc.reverse
       else {
-        val current = s2.head
-        if (previous == current) grouper(s2.drop(1), current, current.toString + acc, result)
-        else grouper(s2.drop(1), current, current.toString, acc :: result)
+        val (group, rest) = remaining.span(_ == remaining.head)
+        rec(rest, group :: acc)
       }
     }
-    val encoding = grouper(s1, s1.head)
-      .map(group => if (group.length == 1) group.head.toString else s"${group.length}${group.head}")
 
-    // If last character is a single character, omit it from the result.
-    (if (encoding.last.length == 1) encoding.init else encoding).mkString("")
+    rec(s, Nil)
+      .map(group => if (group.length == 1) group.head.toString else s"${group.length}${group.head}")
+      .mkString("")
   }
 
-  println(rle("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") == "12WB12W3B24W")
+  println(rle(s))
 }
